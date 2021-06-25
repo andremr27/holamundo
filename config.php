306608f9697b57cc2edbc21c5b81d1647fc66c97
andremr27 <86471357+andremr27@users.noreply.github.com>
2021-06-25 13:05:36 +0200

    Update config.php
    He cambiado la zona horaria Madrid por Bruselas

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 
  define( 'SITE_URL', 'http://andrea.ifcd0211.tk/ejercicios-php/mi-proyecto-V5'); //en producción sería otra url
 
- define( 'SITE_TIMEZONE', 'Europe/Madrid');
+ define( 'SITE_TIMEZONE', 'Europe/Brussels');
  define( 'SITE_LANG', ['es', 'spa', 'es_ES'] );
 
  define( 'DB_HOST', 'localhost' );
